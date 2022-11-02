2351f30b4d84a981a45884e397eb57335cc2257c
rainskin <homemadepunk@yandex.ru>
2022-11-02 15:17:23 +0300

    ..

--- src/config.py
+++ src/config.py
@@ -4,6 +4,6 @@ env.read_envfile()
 
 API_ID = env('API_ID')
 API_HASH = env('API_HASH')
-ADMIN_CHAT_ID = -1001738507553
+ADMIN_CHAT_ID = env.int('ADMIN_CHAT_ID')
 
 BLACKLIST_URLS = env.list('BLACKLIST_URLS')
