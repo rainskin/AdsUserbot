fb9f228e09dc6b00a2a58510d2743126133a0a3f
levch <levchenko.d.a1998@gmail.com>
2023-01-03 01:04:22 +0600

    v 0.1

--- src/handlers/blacklist_urls.py
+++ src/handlers/blacklist_urls.py
@@ -7,10 +7,12 @@ from core import bot
 
 
 @bot.on_message(filters.chat(config.BLACKLIST_URLS_CHAT_ID))
-def save_blacklist_urls(_, message: types.Message):
-    new_urls = lib.urls.parse(message)
+def save_blacklist_urls(_, msg: types.Message):
     saved_urls = {i.value for i in models.BlacklistUrl.get_docs()}
+    new_urls = [i for i in lib.urls.parse(msg) if i not in saved_urls]
 
     for url in new_urls:
-        if url not in saved_urls:
-            models.BlacklistUrl(value=url).save()
+        models.BlacklistUrl(value=url).save()
+
+    urls_text = '\n'.join(new_urls)
+    msg.reply_text(f'Добавлены ссылки:\n\n{urls_text}')
