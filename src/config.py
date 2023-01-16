6c45f9a427cb178850f26aa1ae91086d62801cc0
levch <levchenko.d.a1998@gmail.com>
2023-01-16 20:44:23 +0600

    v 0.3

--- src/config.py
+++ src/config.py
@@ -2,4 +2,10 @@ from core import env
 
 ADMIN_CHAT_ID = env.get_int('ADMIN_CHAT_ID')
 BLACKLIST_URLS_CHAT_ID = env.get_int('BLACKLIST_URLS_CHAT_ID')
-BLACKLIST_URL_REGEXPS = [r'.*telegra.ph.*', '.*t.me/addstickers.*', '.*teletype.in.*']
+BLACKLIST_URL_REGEXPS = [
+    '.*telegra.ph.*',
+    '.*teletype.in.*',
+    '.*t.me/addstickers.*',
+    '.*t.me/addtheme.*',
+    '.*t.me/setlanguage.*',
+]
