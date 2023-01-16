d79cbe453a53002b40725c6bacb5c8859c59a8ce
levch <levchenko.d.a1998@gmail.com>
2023-01-16 20:45:15 +0600

    v 0.3

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ BLACKLIST_URLS_CHAT_ID = env.get_int('BLACKLIST_URLS_CHAT_ID')
 BLACKLIST_URL_REGEXPS = [
     '.*telegra.ph.*',
     '.*teletype.in.*',
-    '.*t.me/addstickers.*',
-    '.*t.me/addtheme.*',
-    '.*t.me/setlanguage.*',
+    '.*t.me/addtheme/.*',
+    '.*t.me/addstickers/.*',
+    '.*t.me/setlanguage/.*',
 ]
